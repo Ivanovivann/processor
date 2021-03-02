177512ecb4f64b9bc70648d3479653db19af4b13
Ivanovivann <70839546+Ivanovivann@users.noreply.github.com>
2021-03-03 02:04:31 +0300

    .

--- asm.cpp
+++ asm.cpp
@@ -21,12 +21,20 @@ typedef struct{
 size_t size_of_file (FILE* text);
 
 buff* reading_file (buff* text, char* name_of_file);
+    
+void filling_buffer_without_tabs_and_other_symbols(char* intermediate_buffer, buff* buffer);
 
 void assembling (buff* buffer);
+
+void filling_labels (char* begin_buf, buff* buffer, label* labels);
+
+void filling_commands(char* begin_buf, buff* buffer, label* labels, double* commands, unsigned count_labels);
 
 void one_argument_handler (double* commands, buff* buffer, int command_number, unsigned* address, label* labels, unsigned count_labels);
 
 void no_arguments_handler (double* commands, buff* buffer, int command_number, unsigned* address, label* labels, unsigned count_labels);
+
+void print_in_file (double* arr_of_commands, int size);
 
 void free_buffer (buff* buffer);
 
@@ -76,15 +84,49 @@ buff* reading_file (buff* buffer, char* name_of_file)
 
     size_t size = size_of_file(input);
 
+    char* intermediate_buffer = (char*) calloc (size, sizeof(char));
     buffer->text = (char*) calloc (size, sizeof(char));
     buffer->size = size;
     assert(buffer->text);
+    assert(intermediate_buffer);
 
-    fread(buffer->text, sizeof(char), size, input);
+    fread(intermediate_buffer, sizeof(char), size, input);
 
     fclose(input);
 
+    filling_buffer_without_tabs_and_other_symbols(intermediate_buffer, buffer);
+
+    free(intermediate_buffer);
+
     return buffer;
+}
+
+//------------------------------------------------------------------------------------------------------
+
+void filling_buffer_without_tabs_and_other_symbols(char* intermediate_buffer, buff* buffer)
+{
+    int current_size = 1;
+    int i = 0;
+
+    while (intermediate_buffer[i] == 9 || intermediate_buffer[i] == ' ')
+        i++;
+
+    for(; i < buffer->size; i++)
+    {
+        if (intermediate_buffer[i] == 9 || intermediate_buffer[i] == '\r') //TAB, \r 
+        {
+            continue;
+        }
+        if ((intermediate_buffer[i] == ' ' || intermediate_buffer[i] == '\n') && buffer->text[current_size - 1] == '\n')
+        {
+            continue;
+        }
+        buffer->text[current_size] = intermediate_buffer[i];
+        current_size++;
+    }
+    buffer->text = (char*) realloc(buffer->text, (current_size + 1) * sizeof(char));
+    buffer->size = current_size + 1;
+    buffer->text[current_size] = '\n';
 }
 
 //------------------------------------------------------------------------------------------------------
@@ -92,7 +134,7 @@ buff* reading_file (buff* buffer, char* name_of_file)
 void one_argument_handler (double* commands, buff* buffer, int command_number, unsigned* address, label* labels, unsigned count_labels)
 {
     buffer->text += strlen(strtok(buffer->text, " ")) + 1;
-    char* token = strtok(buffer->text, "\r");
+    char* token = strtok(buffer->text, "\n");
 
     if (command_number == 5)
     {
@@ -106,7 +148,7 @@ void one_argument_handler (double* commands, buff* buffer, int command_number, u
             commands[(*address)++] = 50;
             commands[(*address)++] = atof(token);
         }
-        buffer->text += strlen(token) + 1;
+        buffer->text += strlen(token);
         return;
     }
 
@@ -123,14 +165,14 @@ void one_argument_handler (double* commands, buff* buffer, int command_number, u
             }
             labels[i].name[strlen(labels[i].name)] = ':';
         }
-        buffer->text += strlen(token) + 1;
+        buffer->text += strlen(token);
         return;
     }
 
     commands[(*address)++] = command_number;
     commands[(*address)++] = atof(token);
 
-    buffer->text += strlen(token) + 1;   
+    buffer->text += strlen(token);   
 }
 
 //------------------------------------------------------------------------------------------------------
@@ -147,15 +189,15 @@ void no_arguments_handler (double* commands, buff* buffer, int command_number, u
         }
         else
         {
-            buffer->text -= strlen(strtok(buffer->text, " ")) + 1;
+            buffer->text -= strlen(strtok(buffer->text, " "));
             commands[(*address)++] = 60;
         }
         
-        buffer->text += strlen(strtok(buffer->text, "\r")) + 1;
+        buffer->text += strlen(strtok(buffer->text, "\n"));
         return;
     }
 
-    buffer->text += strlen(strtok(buffer->text, "\r")) + 1;
+    buffer->text += strlen(strtok(buffer->text, "\n"));
     commands[(*address)++] = command_number;
 }
 
@@ -166,30 +208,51 @@ void assembling (buff* buffer)
     char* token = buffer->text;
     char* begin_buf = buffer->text;
 
-    unsigned current_address = 0;
-    int current_label_number = 0;
     unsigned count_labels = 0;
     unsigned count_commands = 0;
 
-    for (int i = 0; i < buffer->size; i++)
+    buffer->text++;
+
+    for (int i = 0; i < buffer->size - 1; i++)
     {
         if (buffer->text[i] == ':')
             count_labels++;
-        if (buffer->text[i] == '\r' || buffer->text[i] == ' ')
+        if (buffer->text[i] == '\n' || buffer->text[i] == ' ')
             count_commands++;
     }
 
     label* labels = (label*) calloc (count_labels, sizeof(label));
     double* commands = (double*) calloc(count_commands, sizeof(double));
 
-    while (begin_buf + buffer->size - buffer->text > 0 && (token = strtok(buffer->text, "\r")) != NULL)
+    filling_labels(begin_buf, buffer, labels);
+
+    filling_commands(begin_buf, buffer, labels, commands, count_labels);
+
+    print_in_file(commands, count_commands - count_labels);
+
+    buffer->text = begin_buf;
+    free(labels);
+    free(commands);
+
+    return;
+}
+
+//------------------------------------------------------------------------------------------------------
+
+void filling_labels (char* begin_buf, buff* buffer, label* labels)
+{
+    char* token = NULL;
+    unsigned current_address = 0;
+    unsigned current_label_number = 0;
+    
+    while (begin_buf + buffer->size - buffer->text - 1 > 0 && (token = strtok(buffer->text, "\n")) != NULL)
     {
         if (token[strlen(token) - 1] == ':')
         {
             labels[current_label_number].name = token;
             labels[current_label_number].address = current_address;
             current_label_number++;
-            buffer->text += strlen(token) + 1;
+            buffer->text += strlen(token);
         }
         else
         {
@@ -200,18 +263,24 @@ void assembling (buff* buffer)
             }
             current_address++;
 
-            buffer->text += strlen(token) + 1;
+            buffer->text += strlen(token);
         }
         buffer->text++;
     }
-    buffer->text = begin_buf;
+    buffer->text = begin_buf + 1;
+}
 
-    FILE* log = fopen("log_asm.txt", "w");
+//------------------------------------------------------------------------------------------------------
+
+void filling_commands(char* begin_buf, buff* buffer, label* labels, double* commands, unsigned count_labels)
+{
+    char* token = NULL;
     int current_line = 1;
-    current_address = 0;
-    current_label_number = 0;
-    
-    while (begin_buf + buffer->size - buffer->text > 0 && (token = strtok(buffer->text, "\r")) != NULL)
+    unsigned current_address = 0;
+    int current_label_number = 0;
+    FILE* log = fopen("log_asm.txt", "w");
+
+    while (begin_buf + buffer->size - buffer->text - 1 > 0 && (token = strtok(buffer->text, "\n")) != NULL)
     {
         int flag = 0;
         #define CPU(name_of_command, name_code_of_command, code_of_command, in_handler, out_handler, cpu_func)  \
@@ -226,7 +295,7 @@ void assembling (buff* buffer)
         if (token[strlen(token) - 1] == ':')
         {
             flag++;
-            buffer->text += strlen(token) + 1;
+            buffer->text += strlen(token);
         }
         
         if (!flag)
@@ -238,22 +307,7 @@ void assembling (buff* buffer)
 
         buffer->text++;
     }
-
-    FILE* output = fopen("asm_out.txt", "w");
-    for(int i = 0; i <= count_commands - count_labels; i++)
-    {
-        fprintf(output, "%lg\n", commands[i]);
-
-    }
-
-    fclose(output);
     fclose(log);
-
-    buffer->text = begin_buf;
-    free(labels);
-    free(commands);
-
-    return;
 }
 
 //------------------------------------------------------------------------------------------------------
@@ -280,4 +334,3 @@ void free_buffer (buff* buffer)
     free(buffer->text);
     free(buffer);
 }
-
