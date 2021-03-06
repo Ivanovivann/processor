6a0ad2bd324fb8f868792241fc909d8d17d87437
Ivanovivann <70839546+Ivanovivann@users.noreply.github.com>
2021-03-06 22:12:05 +0300

    Update disasm.cpp

--- disasm.cpp
+++ disasm.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 #include <assert.h>
 
 //------------------------------------------------------------------------------------------------------
@@ -10,6 +11,11 @@ typedef struct buff{
     size_t size;
 }buff;
 
+typedef struct{
+    char* name;
+    int address;
+}label;
+
 //------------------------------------------------------------------------------------------------------
 
 size_t size_of_file (FILE* text);
@@ -18,9 +24,11 @@ buff* reading_file (buff* text, char* name_of_file);
 
 void disassembling (buff* buffer);
 
-void func_without_arguments (FILE* output, buff* buffer, char* command_name);
+void filling_labels (char* begin_buf, buff* buffer, label* labels);
 
-void func_with_arguments (FILE* output, buff* buffer, char* command_name);
+void func_without_arguments (FILE* output, buff* buffer, const char* command_name, int counter);
+
+void func_with_one_argument (FILE* output, buff* buffer, const char* command_name, int counter);
 
 void free_buffer (buff* buffer);
 
@@ -87,43 +95,88 @@ void disassembling (buff* buffer)
 {
     char* token = buffer->text;
     char* begin_buf = buffer->text;
-    
+    int counter = 0;
     FILE* output = fopen("disasm_out.txt", "w");
+    label* labels = (label*) calloc (1, sizeof(label));
+    
+    filling_labels (begin_buf, buffer, labels);
     
     while (begin_buf + buffer->size - buffer->text > 0 && (token = strtok(buffer->text, "\n")) != NULL)
     {
         buffer->text += strlen(token) + 1;
         switch (atoi(token)) 
         {
-        #define CPU(name_of_command, name_code_of_command, code_of_command, in_handler, out_handler)    \
-        case code_of_command :                                                                          \
-            out_handler(output, buffer, name_of_command);                                               \
-            break;                                        
-        #include "commands.h"
-        #undef CPU
+            #define CPU(name_of_command, name_code_of_command, code_of_command, in_handler, out_handler, cpu_func)  \
+            case code_of_command :                                                                                  \
+                out_handler(output, buffer, name_of_command, counter);                                                       \
+                break;                                        
+            #include "commands.h"
+            #undef CPU
         }
     }
 
     fclose(output);
 
     buffer->text = begin_buf;
+    free(labels);
 
     return;
 }
 
 //------------------------------------------------------------------------------------------------------
 
-void func_without_arguments (FILE* output, buff* buffer, char* command_name)
+void filling_labels (char* begin_buf, buff* buffer, label* labels)
 {
+    char* token = NULL;
+    unsigned current_address = 0;
+    unsigned current_label_number = 0;
+    
+    while (begin_buf + buffer->size - buffer->text - 1 > 0 && (token = strtok(buffer->text, "\n")) != NULL)
+    {
+        buffer->text++;
+    }
+    buffer->text = begin_buf;
+}
+
+//------------------------------------------------------------------------------------------------------
+
+void func_without_arguments (FILE* output, buff* buffer, const char* command_name, int counter);
+{
+    ++counter;
+    if (!strcmp("classic_pop", command_name))
+    {
+        fprintf(output, "pop\n");
+        return;
+    }
     fprintf(output, "%s\n", command_name);
 }
 
 //------------------------------------------------------------------------------------------------------
 
-void func_with_arguments (FILE* output, buff* buffer, char* command_name)
-{ 
-    fprintf(output, "%s %lg\n", command_name, atof(strtok(buffer->text, "\n")));
-    buffer->text += strlen(strtok(buffer->text, "\n")) + 1;
+void func_with_one_argument (FILE* output, buff* buffer, const char* command_name, int counter);
+{
+    ++++counter;
+    char* token = strtok(buffer->text, "\n");
+    buffer->text += strlen(token) + 1;
+
+    if(!strcmp("reg_pop", command_name))
+    {
+        //printf("pop reg = %s", token);
+        fprintf(output, "pop %cx\n", token[0] + 'a' - '0');
+        return;
+    }
+    if(!strcmp("reg_push", command_name))
+    {
+        //printf("push reg = %s", token);
+        fprintf(output, "push %cx\n", token[0] + 'a'- '0');
+        return;
+    }
+    if(!strcmp("number_push", command_name))
+    {
+        fprintf(output, "push %s\n", token);
+        return;
+    }
+    fprintf(output, "%s %s\n", command_name, token);
 }
 
 //------------------------------------------------------------------------------------------------------
